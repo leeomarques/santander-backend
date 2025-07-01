592d9f7e7dd80ce605aaba0540e80c589681e93c
Leonardo Marques <leonardogm_16@hotmail.com>
2025-07-01 10:00:09 -0300

    Initial commit

---
+++ src/test/java/me/dio/santander_backend/SantanderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
